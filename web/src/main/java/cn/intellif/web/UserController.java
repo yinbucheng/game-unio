de10e5ecc116afc4b46dcc2abc0d70841f259a83
yinchong <yin.chong@intellif.com>
2018-05-23 17:05:41 +0800

    bug修复

--- web/src/main/java/cn/intellif/web/UserController.java
+++ web/src/main/java/cn/intellif/web/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 //    @Autowired
-    @Reference
+    @Reference(version = "1.0")
     private IUserService userService;
 
     @RequestMapping("/save")
